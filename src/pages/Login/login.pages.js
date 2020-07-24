065fd92b97db2450fab197541edf6b3d21015b3e
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2020-07-24 21:33:11 +0700

    :construction: feat: Login

---
+++ src/pages/Login/login.pages.js
@@ -0,0 +1,123 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+import { withRouter } from 'react-router-dom'
+
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+
+import { useFirebase } from 'react-redux-firebase'
+
+import styles from './styles'
+
+import {Copyright} from './../../components'
+
+const useStyles = styles()
+
+function Login() {
+  const classes = useStyles()
+
+  const firebase = useFirebase()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [remember, setRemember] = useState(false)
+
+
+  const handleEmailChange = e => setEmail(e.target.value)
+  const handlePasswordChange = e => setPassword(e.target.value)
+  const handleRememberChange = e => setRemember(e.target.checked)
+
+  const handleFormSubmit = e => {
+    e.preventDefault()
+    firebase.auth().signInWithEmailAndPassword(email, password)
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Log In
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleFormSubmit}>
+          <TextField
+            value={email}
+            onChange={handleEmailChange}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            value={password}
+            onChange={handlePasswordChange}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={remember} onChange={handleRememberChange} color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Log In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  )
+}
+
+export default withRouter(Login)
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    replace: PropTypes.func.isRequired,
+  }).isRequired,
+}
