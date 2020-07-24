57af77337e3b5bb858d74e04567d1e62aa1d2812
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2020-07-24 21:59:15 +0700

    :lipstick: fix: sidebar icon

--- src/layouts/Dashboard/listItems.js
+++ src/layouts/Dashboard/listItems.js
@@ -2,13 +2,9 @@ import React from 'react'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-import ListSubheader from '@material-ui/core/ListSubheader'
 import DashboardIcon from '@material-ui/icons/Dashboard'
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
-import PeopleIcon from '@material-ui/icons/People'
-import BarChartIcon from '@material-ui/icons/BarChart'
-import LayersIcon from '@material-ui/icons/Layers'
-import AssignmentIcon from '@material-ui/icons/Assignment'
+import CategoryRoundedIcon from '@material-ui/icons/CategoryRounded'
+import BallotRoundedIcon from '@material-ui/icons/BallotRounded'
 
 export const mainListItems = (
   <div>
@@ -20,15 +16,15 @@ export const mainListItems = (
     </ListItem>
     <ListItem button>
       <ListItemIcon>
-        <ShoppingCartIcon />
+        <BallotRoundedIcon />
       </ListItemIcon>
-      <ListItemText primary="Product Categories" />
+      <ListItemText primary="Products" />
     </ListItem>
     <ListItem button>
       <ListItemIcon>
-        <PeopleIcon />
+        <CategoryRoundedIcon />
       </ListItemIcon>
-      <ListItemText primary="Products" />
+      <ListItemText primary="Product Categories" />
     </ListItem>
   </div>
 )
